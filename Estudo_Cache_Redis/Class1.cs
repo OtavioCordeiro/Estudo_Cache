48745b1ca0fe76d7eb12bdf0bf92ee06f11848d9
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-01-28 19:13:34 -0200

    Add project files.

---
+++ Estudo_Cache_Redis/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_Cache_Redis
+{
+    public class Class1
+    {
+    }
+}
